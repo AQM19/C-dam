397e7bc9feec0dbdc4f6dc22b90cbfcff78c7c53
DESKTOP-RM01CT1\aaron <quintanalaaron@gmail.com>
2023-10-05 22:38:00 +0200

    delete

--- Examen_AaronQuintanal/app/src/main/java/es/aaronquintanal/examen_aaronquintanal/modelo/Equipo.kt
+++
@@ -1,7 +0,0 @@
-package es.aaronquintanal.examen_aaronquintanal.modelo
-
-import android.media.Image
-import android.widget.ImageView
-import java.io.Serializable
-
-data class Equipo (val pais : String, val descripcion : String, val bandera : Int, val imagen : Int, var pasa : Boolean = false) : Serializable
